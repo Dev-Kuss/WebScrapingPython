540e3c93ffde0c653c2ba8b48f778f01e7fb7246
Dev-Kuss <mkuss.dev@gmail.com>
2024-05-09 10:21:34 -0300

    gravando no CSV

--- Agro/PrecoSojaAbelardoLuz-Coamo.py
+++ Agro/PrecoSojaAbelardoLuz-Coamo.py
@@ -1,4 +1,5 @@
 import time
+import csv
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
@@ -7,6 +8,7 @@ from bs4 import BeautifulSoup
 
 initial_url = "https://coamo.com.br/preco-do-dia"
 City = "ABELARDO LUZ"
+Product = "SOJA"
 
 driver = webdriver.Chrome()
 driver.get(initial_url)
@@ -29,14 +31,22 @@ if table_element:
         cols = row.find_all("td")
         if len(cols) >= 5:
             cols_data = [col.text.strip() for col in cols]
-            if cols_data[0].strip() == "SOJA":
+            if Product == "SOJA":
+
                 price = cols_data[3].replace("R$", "").replace("\xa0", "").replace(",", ".").strip()
                 date_string = cols_data[2]
-                dt = datetime.strptime(date_string, "%d/%m/%Y %H:%M")  # assuming the format is DD/MM/YYYY HH:MM
+                dt = datetime.strptime(date_string, "%d/%m/%Y %H:%M")
+                unidade_medida = cols_data[4]
+
+                with open('results.csv', 'a', newline='') as f:
+                    writer = csv.writer(f)
+                    writer.writerow([Product, dt.strftime('%Y-%m-%d %H:%M'), price, unidade_medida])
+
+
                 print(f"Produto: {cols_data[0]}")
                 print(f"Preço: {price}")
                 print(f"Data e Hora do Preço: {dt.strftime('%Y-%m-%d %H:%M')}")
-                print(f"Unid. Padrão: {cols_data[4]}")
+                print(f"Unid. Padrão: {unidade_medida}")
                 driver.quit()
                 break
 else:
